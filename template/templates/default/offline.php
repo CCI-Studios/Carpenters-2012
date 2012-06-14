877c25a5c6b50dce84ad0f3928b5f153b1706da4
Jonathan Bennett <jbennett@ccistudios.com>
2012-06-13 23:39:48 -0400

    analytics is only included if used
    typo, should have just been null

--- template/templates/default/offline.php
+++ template/templates/default/offline.php
@@ -25,7 +25,7 @@ if ($_SERVER['SERVER_PORT'] === 8888 ||
 }
 
 JHTML::_('behavior.mootools');
-$analytics = "UA-XXXXX-X"; // FIXME Update to client ID
+$analytics = null; // FIXME Update to client ID
 $typekit = null;
 ?>
 
